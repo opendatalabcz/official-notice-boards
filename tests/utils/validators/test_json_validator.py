f6885cf564298c033b92f7829e768448a4c632c4
Jakub Kucera <kucerj56@fit.cvut.cz>
2022-02-11 15:46:12 +0100

    Add tests for xml validator

--- tests/utils/validators/test_json_validator.py
+++ tests/utils/validators/test_json_validator.py
@@ -7,23 +7,23 @@ from src.utils.validators.json_validator import JsonValidator
 DATA_DIR_PATH = "utils/validators/data"
 
 
-def convert_to_full_path(file_path: str) -> str:
-    return os.path.join(DATA_DIR_PATH, file_path)
+def convert_to_full_path(file_name: str) -> str:
+    return os.path.join(DATA_DIR_PATH, file_name)
 
 
 @pytest.mark.parametrize(
-    "file_path",
+    "file_name",
     (
         "valid_json.json",
         "valid_empty_json.json",
     ),
 )
-def test_validate_success(file_path: str) -> None:
-    assert JsonValidator.validate(convert_to_full_path(file_path))
+def test_validate_success(file_name: str) -> None:
+    assert JsonValidator.validate(convert_to_full_path(file_name))
 
 
 @pytest.mark.parametrize(
-    "file_path",
+    "file_name",
     (
         "valid_xml.xml",
         "invalid_file_path.json",
@@ -32,5 +32,5 @@ def test_validate_success(file_path: str) -> None:
         # "utils/validators/data/valid_json.xml",
     )
 )
-def test_validate_fail(file_path: str) -> None:
-    assert not JsonValidator.validate(convert_to_full_path(file_path))
+def test_validate_fail(file_name: str) -> None:
+    assert not JsonValidator.validate(convert_to_full_path(file_name))
