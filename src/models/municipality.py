3f7771ab127da4076b7ffd75899aaa41b17a5d6e
Jakub Kucera <kucerj56@fit.cvut.cz>
2022-02-13 18:46:47 +0100

    Modify Municipality model.
    In Municipality model make RUIAN a primary key. Add 1:N relation with itself,
    this is mainly done because some municipality parts don't have their own ICO,
    so they use the one of whole Municipality.

--- src/models/municipality.py
+++ src/models/municipality.py
@@ -1,16 +1,29 @@
+from typing import Optional
+
 from src.app import db
+from src.models.mapper import Mapper
 
 
 class Municipality(db.Model):
     """
     Municipality model
     """
-    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=False, nullable=False)
-    ruian = db.Column(db.Integer, unique=True, nullable=False)
-    ico = db.Column(db.Integer, unique=True, nullable=False)  # nullable?
-    # only_municipality_part = db.Column(db.Boolean, unique=False, nullable=False)  # TODO maybe
+    ruian = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    # municipality parts aside from Prague parts use their mains municipality office ico
+    ico = db.Column(db.Integer, unique=True, nullable=True)
+    parent_ruian = db.Column(db.Integer, db.ForeignKey('municipality.ruian'))
+    parent = db.relationship("Municipality", remote_side=[ruian])
 
     def __repr__(self):
-        return f"<Municipality(id='{self.id}', name='{self.name}'," \
-                             f"ruian='{self.ruian}, ico='{self.ico}')>"
+        return f"<Municipality(name='{self.name}', ruian='{self.ruian}, ico='{self.ico}')>"
+
+    @classmethod
+    def extract_from_dict(cls, data):
+        name = data['nazev']['cs']
+        ruian = data['kod']
+        ico = Mapper.get_ico(ruian)
+        parent_ruian: Optional[str] = None
+        if 'obec' in data:
+            parent_ruian = data['obec'].split(sep='/')[-1]
+        return cls(name=name, ruian=ruian, ico=ico, parent_ruian=parent_ruian)
