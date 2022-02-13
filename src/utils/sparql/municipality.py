3cb0d6b2048ecd91feb6b13bb6babd455cef4f67
Jakub Kucera <kucerj56@fit.cvut.cz>
2022-02-13 18:24:57 +0100

    Add sparql query to get url for list of municipalities. Add function that downloads data from the url.

---
+++ src/utils/sparql/municipality.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+import requests
+
+from src.utils.sparql.endpoint import Endpoint
+from src.utils.sparql.query import run_query
+
+_MUNICIPALITY_LIST_DESCRIPTION = "Číselník obcí ČR."
+_MUNICIPALITY_PART_LIST_DESCRIPTION = "Číselník městských obvodů a městských částí"
+
+_GET_DOWNLOAD_LIST_QUERY = \
+    """
+    SELECT ?download_url WHERE
+    {{
+        ?municipalities_list dct:description  "{description}"@cs;
+                             dcat:distribution ?distribution.
+        ?distribution dct:format ?format;
+                      dcat:downloadURL ?download_url.
+        ?format dc:identifier "JSON".
+    }}
+    """
+
+
+def _fetch_list_url(is_part: bool) -> str:
+    description = _MUNICIPALITY_PART_LIST_DESCRIPTION if is_part else _MUNICIPALITY_LIST_DESCRIPTION
+    query = _GET_DOWNLOAD_LIST_QUERY.format(description=description)
+    result = run_query(query, Endpoint.DATA_GOV)
+    url = result['results']['bindings'][0]['download_url']['value']
+    return url
+
+
+def fetch_municipality_list(is_part: bool) -> list[dict[Any, Any]]:
+    url = _fetch_list_url(is_part)
+    return requests.get(url).json()['polozky']
