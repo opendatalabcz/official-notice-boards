8cbb9062b854f5cab8775ab50269361ff4c754d8
Jakub Kucera <kucerj56@fit.cvut.cz>
2022-03-15 02:23:08 +0100

    Switch from using SQLite to Postres

--- src/app.py
+++ src/app.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///official_notice_boards.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:example@localhost:5432/postgres'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/postgres'
+# app.config['PAS']
+# TODO replace by env var, either .env file or in docker-compose
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///official_notice_boards.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
