e60d74e5bd5fe79fbb715758b5a9d30bfe776aef
Jakub Kucera <kucerj56@fit.cvut.cz>
2022-03-20 01:41:10 +0100

    Delete file validators and associated unit tests

--- src/utils/validators/__init__.py
+++
@@ -1,5 +0,0 @@
-# TODO maybe add import for all validators here
-# from .csv_validator import CsvValidator  # noqa
-# from .json_validator import JsonValidator  # noqa
-# from .jsonld_validator import JsonLdValidator  # noqa
-# from .xml_validator import XmlValidator  # noqa
--- src/utils/validators/file_type.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum, auto
-
-
-class FileType(Enum):
-    CSV = auto()
-    JSON = auto()
-    JSON_LD = auto()
-    XML = auto()
-
