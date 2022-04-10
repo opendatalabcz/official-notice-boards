2bccbea46b383d4332090f322be38af95e488d39
Jakub Kucera <kucerj56@fit.cvut.cz>
2022-04-10 17:35:55 +0200

    Add support for DB migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap5
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     db.init_app(app)
+    migrate.init_app(app, db)
     bootstrap = Bootstrap5(app)
     # logging.basicConfig(level=logging.INFO)
 
