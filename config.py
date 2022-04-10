379a24c8d689a21d205df9e557b92026831c94b4
Jakub Kucera <kucerj56@fit.cvut.cz>
2022-04-10 16:27:39 +0200

    Move DATABASE URI to .env file for better user modification

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
 class Config:
-    TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:example@localhost:5432/postgres'
+    # TESTING = False
+    # DEVELOPMENT = False
+    SQLALCHEMY_DATABASE_URI = environ.get('PROD_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     BOOTSTRAP_BOOTSWATCH_THEME = 'Flatly'
