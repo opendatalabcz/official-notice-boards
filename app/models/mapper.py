1fb58fa3b9be4d72c83ebed851b6346da0533af7
Jakub Kucera <kucerj56@fit.cvut.cz>
2022-04-10 16:22:57 +0200

    Revert Mapper changes
    Delete latitude and longitude attributes from Mapper model

--- app/models/mapper.py
+++ app/models/mapper.py
@@ -14,24 +14,17 @@ class Mapper(db.Model):
     """
     ruian = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     ico = db.Column(db.Integer, unique=True, nullable=True)  # , index=True)
-    location_latitude = db.Column(db.Float, unique=False, nullable=True)
-    location_longitude = db.Column(db.Float, unique=False, nullable=True)
 
     def __repr__(self):
-        return f"<Mapper(ico={self.ico}, ruian={self.ruian}, " \
-               f"location_latitude={self.location_latitude}, " \
-               f"location_longitude={self.location_longitude})>"
+        return f"<Mapper(ico={self.ico}, ruian={self.ruian})>"
 
     @classmethod
     def extract_from_dict(cls, data):
         ruian = nested_get(data, ['ruian', 'value'])
-        location = nested_get(data, ['location', 'value'])
-        # location_latitude, location_longitude = re.findall("Point\((.+)\)", location)[0].split(' ')
-        location_latitude, location_longitude = None, None
         ico = None
         if 'ico' in data:
             ico = nested_get(data, ['ico', 'value'])
-        return cls(ruian=ruian, ico=ico, location_latitude=location_latitude, location_longitude=location_longitude)
+        return cls(ruian=ruian, ico=ico)
 
     @classmethod
     def get_ico(cls, ruian: int) -> Optional[int]:
